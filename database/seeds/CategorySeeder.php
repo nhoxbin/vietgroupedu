91827abc3fbd133d5c550b4cc309bc3036ccf4fb
nhoxbin <khoaij123@gmail.com>
2020-09-26 12:06:37 +0700

    Second commit

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin_menu')->truncate();
+        DB::table('admin_menu')->insert([
+        	[
+        		'parent_id' => 0,
+        		'order' => 8,
+        		'title' => 'Danh mục',
+        		'icon' => 'fa-code-fork',
+        		'uri' => 'categories'
+        	],
+            [
+                'parent_id' => 0,
+                'order' => 9,
+                'title' => 'Loại bài viết',
+                'icon' => 'fa-asterisk',
+                'uri' => 'post-types'
+            ],
+        	[
+        		'parent_id' => 0,
+        		'order' => 10,
+        		'title' => 'Bài viết',
+        		'icon' => 'fa-pencil-square-o',
+        		'uri' => 'posts'
+        	],
+            [
+                'parent_id' => 0,
+                'order' => 11,
+                'title' => 'File Manager',
+                'icon' => 'fa-pencil-square-o',
+                'uri' => 'file-manager'
+            ]
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Auth::routes(['register' => false, 'verify' => false]);
+
+Route::get('/{name?}', 'HomeController@index')->where('name', 'home')->name('home');
+
+Route::group(['prefix' => 'artisan/{password}'], function() {
+    Route::get('{command}', function($password, $command) {
+		if ($password === 'buudaica') {
+	        $exitCode = \Artisan::call($command, ['--force' => true]);
+	        echo $exitCode;
+		}
+    });
+});
