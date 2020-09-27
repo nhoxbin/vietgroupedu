156658d4fd37c8952769d56368f952cb1025a100
nhoxbin <khoaij123@gmail.com>
2020-09-27 09:45:23 +0700

    update webfont and other stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -123,7 +123,7 @@
                                                 <ul id="nav" class="border-full border-top">
                                                     <li class="current"><a href="/">Trang Chủ</a></li>
                                                     <li><a href="{{ url('/gioi-thieu') }}">Giới thiệu</a></li>
-                                                    <li><a href="courses.html">Dịch vụ</a>
+                                                    <li><a href="#">Dịch vụ</a>
                                                         <ul class="sub-menu">
                                                             <li><a href="{{ url('/xuat-khau-lao-dong') }}">Xuất khẩu lao động</a></li>
                                                             <li><a href="{{ url('/du-hoc') }}">Du học</a></li>
@@ -137,18 +137,18 @@
                                                         </ul>
                                                     </li>
                                                     <li>
-                                                        <a href="{{ url('/contact') }}">Liên hệ</a>
+                                                        <a href="{{ url('/lien-he') }}">Liên hệ</a>
                                                     </li>
                                                 </ul>
                                             </nav>
                                         </div>
-                                        <ul class="header-search">
+                                        {{-- <ul class="header-search">
                                             <li class="search-menu">
                                                 <i id="toggle-search" class="zmdi zmdi-search-for"></i>
                                             </li>
-                                        </ul>
+                                        </ul> --}}
                                         <!--Search Form-->
-                                        <div class="search">
+                                        {{-- <div class="search">
                                             <div class="search-form">
                                                 <form id="search-form" action="#">
                                                     <input type="search" placeholder="Search here..." name="search" />
@@ -157,7 +157,7 @@
                                                     </button>
                                                 </form>                                
                                             </div>
-                                        </div>
+                                        </div> --}}
                                         <!--End of Search Form-->
                                     </div> 
                                 </div>
@@ -172,120 +172,22 @@
                                     <div class="mobile-menu">
                                         <nav id="dropdown">
                                             <ul>
-                                                <li><a href="/">Trang Chủ</a>
-                                                    <ul>
-                                                        <li><a href="index.html">Slider Style 1</a></li>
-                                                        <li><a href="index-2.html">Slider Style 2</a></li>
-                                                        <li><a href="index-3.html">Background Image</a></li>
-                                                        <li><a href="index-4.html">Image Overlay Light</a></li>
-                                                        <li><a href="index-5.html">Image Overlay Dark</a></li>
-                                                        <li><a href="index-6.html">Menu With Slider</a></li>
-                                                        <li><a href="index-7.html">Menu With Image</a></li>
-                                                        <li><a href="index-8.html">Menu With Overlay</a></li>
-                                                        <li><a href="index-9.html">Video Background</a></li>
-                                                        <li><a href="index-10.html">Fixed Image</a></li>
-                                                        <li><a href="index-11.html">Overlay Fixed Image</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="about.html">About Us</a></li>
-                                                <li><a href="courses.html">Courses</a>
+                                                <li><a href="/">Trang Chủ</a></li>
+                                                <li><a href="{{ url('/gioi-thieu') }}">Giới thiệu</a></li>
+                                                <li><a href="#">Dịch vụ</a>
                                                     <ul class="sub-menu">
-                                                        <li><a href="courses-details.html">Courses Details</a></li>
+                                                        <li><a href="{{ url('/xuat-khau-lao-dong') }}">Xuất khẩu lao động</a></li>
+                                                        <li><a href="{{ url('/du-hoc') }}">Du học</a></li>
+                                                        <li><a href="{{ url('/dao-tao-ngoai-ngu') }}">Đào tạo ngoại ngữ</a></li>
                                                     </ul>
                                                 </li>
-                                                <li><a href="shop-grid.html">Shop</a>
+                                                <li><a href="#">Thông báo</a>
                                                     <ul class="sub-menu">
-                                                        <li><a href="product-details.html">Product Details</a></li>
+                                                        <li><a href="{{ url('/tin-tuc') }}">Tin tức</a></li>
+                                                        <li><a href="{{ url('/su-kien') }}">Sự kiện</a></li>
                                                     </ul>
                                                 </li>
-                                                <li><a href="event.html">Event</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="event-details.html">Event Details</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="index.html">Shortcode</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="shortcode-course.html">Courses</a></li>
-                                                        <li><a href="shortcode-event.html">Event</a></li>
-                                                        <li><a href="shortcode-latest-news.html">Latest News</a></li>
-                                                        <li><a href="shortcode-product.html">Product</a></li>
-                                                        <li><a href="shortcode-testimonial.html">Testimonial</a></li>
-                                                        <li><a href="shortcode-contact-form.html">Contact Form</a></li>
-                                                        <li><a href="shortcode-map.html">Map</a></li>
-                                                        <li><a href="shortcode-facts.html">Facts</a></li>
-                                                        <li><a href="image-gallery.html">Image Gallery</a></li>
-                                                        <li><a href="video-gallery.html">Video Gallery</a></li>
-                                                        <li><a href="shortcode-alerts.html">Alerts</a></li>
-                                                        <li><a href="shortcode-button.html">Button</a></li>
-                                                        <li><a href="shortcode-breadcrumbs.html">Breadcrumb</a></li>
-                                                        <li><a href="shortcode-dropdown.html">Dropdown</a></li>
-                                                        <li><a href="shortcode-pagination.html">Pagination</a></li>
-                                                        <li><a href="shortcode-progressbar.html">Progressbar</a></li>
-                                                        <li><a href="text-animation-1.html">Text Animation 1</a></li>
-                                                        <li><a href="text-animation-2.html">Text Animation 2</a></li>
-                                                        <li><a href="text-animation-3.html">Text Animation 3</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="latest-news.html">Latest News</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="news-details.html">News Details</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="index.html">Features</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="#">Sliders</a>
-                                                            <ul>
-                                                                <li><a href="slider-style-1.html">Slider Style 1</a></li>
-                                                                <li><a href="slider-style-2.html">Slider Style 2</a></li>
-                                                                <li><a href="slider-style-3.html">Slider Style 3</a></li>
-                                                                <li><a href="background-image.html">Image Background </a></li>
-                                                                <li><a href="image-overlay-light.html">Overlay Light </a></li>
-                                                                <li><a href="image-overlay-dark.html">Overlay Dark </a></li>
-                                                                <li><a href="video-background.html">Video Background </a></li>
-                                                                <li><a href="fixed-image.html">Fixed Image</a></li>
-                                                                <li><a href="overlay-fixed-image.html">Overlay Fixed Image</a></li>
-                                                                <li><a href="text-animation-1.html">Text Animation 1 </a></li>
-                                                                <li><a href="text-animation-2.html">Text Animation 2 </a></li>
-                                                                <li><a href="text-animation-3.html">Text Animation 3 </a></li>
-                                                            </ul>
-                                                        </li>
-                                                        <li><a href="#">Menu Style</a>
-                                                            <ul>
-                                                                <li><a href="theme-menu-1.html">Theme Menu 1</a></li>
-                                                                <li><a href="theme-menu-2.html">Theme Menu 2</a></li>
-                                                                <li><a href="theme-menu-3.html">Theme Menu 3</a></li>
-                                                                <li><a href="without-top-bar.html">Without Top Bar</a></li>
-                                                                <li><a href="menu-center.html">Menu Center</a></li>
-                                                                <li><a href="menu-transparent.html">Transparent</a></li>
-                                                                <li><a href="menu-semi-transparent.html">Semi Transparent</a></li>
-                                                                <li><a href="menu-dark.html">Menu Dark</a></li>
-                                                                <li><a href="borderd-menu.html">Menu Border</a></li>
-                                                                <li><a href="menu-top-border.html">Top Border Hover</a></li>
-                                                                <li><a href="menu-sticky.html">Menu Sticky</a></li>
-                                                                <li><a href="menu-non-sticky.html">Menu Non Sticky</a></li>
-                                                            </ul>
-                                                        </li>
-                                                        <li><a href="#">Page Title</a>
-                                                            <ul>
-                                                                <li><a href="breadcrumb-dark.html">Title Dark</a></li>
-                                                                <li><a href="breadcrumb-fixed.html">Title Fixed</a></li>
-                                                                <li><a href="breadcrumb-image.html">Title Image</a></li>
-                                                                <li><a href="breadcrumb-transparent.html">Title Transparent</a></li>
-                                                                <li><a href="breadcrumb-left.html">Title Left</a></li>
-                                                                <li><a href="breadcrumb-right.html">Title Right</a></li>
-                                                            </ul>
-                                                        </li>
-                                                        <li><a href="#">Footer Style</a>
-                                                            <ul>
-                                                                <li><a href="footer-style-1.html">Footer Style 1</a></li>
-                                                                <li><a href="footer-style-2.html">Footer Style 2</a></li>
-                                                                <li><a href="footer-style-3.html">Footer Style 3</a></li>
-                                                                <li><a href="footer-style-4.html">Footer Style 4</a></li>
-                                                            </ul>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="contact.html">Contact us</a></li>
+                                                <li><a href="{{ url('/lien-he') }}">Liên hệ</a></li>
                                             </ul>
                                         </nav>
                                     </div>					
@@ -521,7 +423,7 @@
                                             <tr>
                                                 <th>Ngành nghề</th>
                                                 <th>Mức lương</th>
-                                                <th>Phỏng vấn</th>
+                                                <th>Ngày phỏng vấn</th>
                                             </tr>
                                         </thead>
                                         <tbody>
